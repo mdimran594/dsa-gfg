9d8cd38217a633cdf24921ffc0870fe5063d5566
Md Imran <61510385+mdimran594@users.noreply.github.com>
2025-03-03 11:10:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    int last_index;
+
+    public ArrayList<Integer> longestSubarray(int[] arr, int x) {
+        // code here
+           this.last_index = 0;
+        int low = 1;
+        int high = arr.length;
+        
+        while(low < high) {
+            int mid = (low + high + 1) / 2;
+            if(checkIfExist(arr , x , mid)) {  // checking is there exist any window of size "k"  with max absolute difference upto "x"
+                low = mid;
+            } else {
+                high = mid-1;
+            }
+        }
+        
+        // System.out.println(low + " " + high);
+        ArrayList<Integer> A = new ArrayList<>();
+        for(int i = this.last_index; i > this.last_index-low && i >= 0; i--) {
+            A.add(0 , arr[i]);
+        }
+        return A;
+    }
+    
+    private boolean checkIfExist(int arr[], int x, int k) {
+        Deque<Integer> min_dq = new LinkedList<>();
+        Deque<Integer> max_dq = new LinkedList<>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            while(!min_dq.isEmpty() && arr[i] <= arr[min_dq.peekFirst()]) {
+                min_dq.pollFirst();
+            }
+            while(!min_dq.isEmpty() && min_dq.peekLast() <= i-k) {
+                min_dq.pollLast();
+            }
+            min_dq.offerFirst(i);  // increasing-order
+            
+            while(!max_dq.isEmpty() && arr[i] >= arr[max_dq.peekFirst()]) {
+                max_dq.pollFirst();
+            }
+            while(!max_dq.isEmpty() && max_dq.peekLast() <= i-k) {
+                max_dq.pollLast();
+            }
+            max_dq.offerFirst(i);   // decreasing-order
+            
+            int min = arr[min_dq.peekLast()];
+            int max = arr[max_dq.peekLast()];
+
+            if(max - min <= x && i >= k-1) {
+                this.last_index = i;
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int k = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.longestSubarray(arr, k);
+
+            // Print the result as a space-separated string
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println(); // New line after printing the results
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
